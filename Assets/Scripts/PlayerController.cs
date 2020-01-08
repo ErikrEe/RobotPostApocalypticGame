dab9b77e8af62810eee5384ee7f30e621e669e28
ErikrEe <56590091+ErikrEe@users.noreply.github.com>
2020-01-08 18:06:30 +0100

    Added Scripts for movement/Jumping + "TestArea"
    -Made a "PlayerController" Script which lets you move left and right + jumping when standing on something that has the tag "Ground"
    
    -Made another script that only checks if the player is standing on a "Ground" and returns a "true or false" value
    
    -Added a placeholder object for the Player/Robot
    
    -Added a test-scene or a test "Area", With a ground(Cube), and a "box"(Cube).
    
    -Also added parents for objects that have the tag "Ground" and Parents for objects that do NOT have the tag "Ground"
    
    -Bug/fix I discovored is when you run into objects and continue moving in that direction, the player starts glitching and vibrating, Don't know how to fix rn

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    public float moveSpeed = 10f;
+    public float jumpHeight = 10f;
+
+    public bool isGrounded = false;
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Jump();
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * moveSpeed;
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded == true   )
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
+        }
+         
+    }
+
+}
