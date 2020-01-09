a0ca70971618c0ee5e232bbb3995551764631f91
ErikrEe <56590091+ErikrEe@users.noreply.github.com>
2020-01-09 13:20:55 +0100

    Added TestScene for Level design

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,7 @@ public class PlayerController : MonoBehaviour
 
     public bool isGrounded = false;
 
+
     Rigidbody2D rb;
 
     // Start is called before the first frame update
@@ -23,8 +24,10 @@ public class PlayerController : MonoBehaviour
     {
         Jump();
 
-        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
-        transform.position += movement * Time.deltaTime * moveSpeed;
+         Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+         transform.position += movement * Time.deltaTime * moveSpeed;
+
+
     }
 
     void Jump()
