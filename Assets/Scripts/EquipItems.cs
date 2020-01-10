02855c02dc03ed9a0b2e67f7e6d00db19fb6eb27
Arex2 <56590254+Arex2@users.noreply.github.com>
2020-01-10 19:27:29 +0100

    New script for equipping items
    New script for equipping items, only works with flower for now, also sometimes doesn't rotate in the right way when equipped and jumpes way higher than the player.

---
+++ Assets/Scripts/EquipItems.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipItems : MonoBehaviour
+{
+    [SerializeField]
+    public Rigidbody2D equObject;
+    public static bool pickedUp = false;
+    Vector2 vector;
+    Vector2 vectorTwo;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vector.x = gameObject.transform.position.x;
+        vectorTwo.y = equObject.transform.position.y;
+        if (Input.GetKeyDown(KeyCode.E) && (gameObject.transform.position - this.transform.position).sqrMagnitude < 1.5f * 1.5f)
+        {
+            vector.x = vector.x + 0.7f; //makes the vector.x equal to itself plus 0.7f
+            vectorTwo.y = vectorTwo.y + 1f; //makes the vector.x equal to itself plus 0.7f
+            equObject.transform.position = new Vector2(vector.x, vectorTwo.y); //Makes the position of the game object equal to the players position plus 0.7f on the x axis
+            equObject.transform.parent = gameObject.transform;
+            pickedUp = true;
+            equObject.transform.rotation = Quaternion.Euler(0, 0, -90);
+            equObject.constraints = RigidbodyConstraints2D.FreezeRotation;
+
+
+
+
+            //GameObject.FindGameObjectsWithTag("equObject")
+        }
+        if(pickedUp)
+        {
+
+            vector.x = vector.x + 0.7f; //makes the vector.x equal to itself plus 0.7f
+            equObject.transform.position = new Vector2(vector.x, vectorTwo.y); //Makes the position of the game object equal to the players position plus 0.7f on the x axis
+           // vectorTwo.y = vectorTwo.y + 1f; //makes the vector.x equal to itself plus 0.7f
+        }
+
+        if (Input.GetKey(KeyCode.Q) && pickedUp)
+        {
+            equObject.transform.parent = null; //this removes the object from being a child to the player 
+            pickedUp = false;
+            equObject.constraints = RigidbodyConstraints2D.None; // Un freezes the object so it can tilt over again.
+
+        }
+    }
+}
--- Assets/Scripts/Flower.cs
+++ Assets/Scripts/Flower.cs
@@ -33,16 +33,17 @@ public class Flower : MonoBehaviour
         }*/
 
 
-        vector.x = player.transform.position.x; //makes the variable vector equal to the players x position 
+      //  vector.x = player.transform.position.x; //makes the variable vector equal to the players x position 
         if (Input.GetKeyDown(KeyCode.E) && (player.transform.position - this.transform.position).sqrMagnitude < 1.5f * 1.5f) //This if statement is triggered whe the button E is pressed and when the player is close enough to the object in a 1.3*1.3 radius from the player.
         {
             vector.x = vector.x + 0.7f; //makes the vector.x equal to itself plus 0.7f
             gameObject.transform.position = new Vector2(vector.x, gameObject.transform.position.y); //Makes the position of the game object equal to the players position plus 0.7f on the x axis
             gameObject.transform.parent = player.transform; //Makes the gameObject a child of the player (player from the serialize field player variable)
-            pickedUp = true;  //changes this bool to true
-            gameObject.transform.rotation = Quaternion.Euler(0, 0, -90); //changes the rotation of the object to 0,0,-90 so that the object is in its default position when picked up
-            flower.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation; //Freezes the rotation and position of the object when it's picked up so it does not fall over or slide away from the player/parent
-            //##flower.constraints = RigidbodyConstraints2D.FreezeRotation; // Freezes the rotation of the object when it's picked up so it does not fall over
+            pickedUp = true;
+            //changes this bool to true
+                    gameObject.transform.rotation = Quaternion.Euler(0, 0, -90); //changes the rotation of the object to 0,0,-90 so that the object is in its default position when picked up
+                    flower.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezeRotation; //Freezes the rotation and position of the object when it's picked up so it does not fall over or slide away from the player/parent
+                   //##flower.constraints = RigidbodyConstraints2D.FreezeRotation; // Freezes the rotation of the object when it's picked up so it does not fall over
         }
 
        /* if(Input.GetKeyDown(KeyCode.A))// här försöker jag ändra så att blomman är på andra sidan av karaktären när den går åt vänster
