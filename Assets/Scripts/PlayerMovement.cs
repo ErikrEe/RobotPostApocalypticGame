334a527211edc11a0c2fd0abddfbde49aa3e6a80
ErikrEe <56590091+ErikrEe@users.noreply.github.com>
2020-01-10 13:38:28 +0100

    Made testScene With good movement
    -Added a testScene for better movement called "NewCodeScene"
    
    -Made two new scripts (Both of them is for controlling the character)
    
    -Commented scripts
    
    -Added new Physics material
    
    -Added A couple things on to the player in the testScene so that it moves smoother, doesn't glitch when hitting walls and can crouch

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController controller; //Ref to characterControllerScript
+
+
+    public float moveSpeed = 40f;
+
+    float xMove = 0f;  //Horizontal movement (Not speed)
+
+    bool jump = false;
+    bool crouch = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        xMove = Input.GetAxisRaw("Horizontal") * moveSpeed; // -1 * moveSpeed |or| +1 * moveSpeed
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            jump = true;  //If player presses "space" or "up" or "W" then it sets "jump" = true
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;  //If player presses "down" or "s" then it sets "crouch" = true
+        }
+
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;  //if player releases the Crouch button, then it sets "crouch" = false
+        }
+
+
+    }
+
+    //Multiplyng by Time.fixedDeltaTime will make sure that we're moving the same amount,
+    //regardless of how many times (per second) this function is called
+
+    private void FixedUpdate()
+    {
+        controller.Move(xMove * Time.fixedDeltaTime, crouch, jump); //Arg 1 = horizontal movement || Arg 2 = Crouching? || Arg 3 = Jumping?
+        jump = false;  //sets jump to false when player has jumped
+
+
+
+    }
+}
