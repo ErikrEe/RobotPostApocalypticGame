a862d6f53e9a61db6363f693548078d2efa9197b
Arex2 <56590254+Arex2@users.noreply.github.com>
2020-01-11 13:06:22 +0100

    Flower now almost fully works
    CHanged the equipItems script, adding a rule which makes the height of the flower the same as the player plus 0.2f.
    
    Having the flower as a child of the player is no longer necessary

--- Assets/Scripts/EquipItems.cs
+++ Assets/Scripts/EquipItems.cs
@@ -19,16 +19,17 @@ public class EquipItems : MonoBehaviour
     void Update()
     {
         vector.x = gameObject.transform.position.x;
-        vectorTwo.y = equObject.transform.position.y;
+        vectorTwo.y = gameObject.transform.position.y;
         if (Input.GetKeyDown(KeyCode.E) && (gameObject.transform.position - this.transform.position).sqrMagnitude < 1.5f * 1.5f)
         {
-            vector.x = vector.x + 0.7f; //makes the vector.x equal to itself plus 0.7f
-            vectorTwo.y = vectorTwo.y + 1f; //makes the vector.x equal to itself plus 0.7f
-            equObject.transform.position = new Vector2(vector.x, vectorTwo.y); //Makes the position of the game object equal to the players position plus 0.7f on the x axis
-            equObject.transform.parent = gameObject.transform;
             pickedUp = true;
+            vector.x = vector.x + 0.7f;                                                 //makes the vector.x equal to itself plus 0.7f
+            //vectorTwo.y = vectorTwo.y + 1f;                                             //makes the vector.x equal to itself plus 0.7f
+           // equObject.transform.position = new Vector2(vector.x, vectorTwo.y);          //Makes the position of the game object equal to the players position plus 0.7f on the x axis
+            equObject.transform.parent = gameObject.transform; //##Den behöver inte bli en child till den andra längre.
             equObject.transform.rotation = Quaternion.Euler(0, 0, -90);
             equObject.constraints = RigidbodyConstraints2D.FreezeRotation;
+            //equObject.GetComponent<Rigidbody2D>().gravityScale = 0;
 
 
 
@@ -37,18 +38,17 @@ public class EquipItems : MonoBehaviour
         }
         if(pickedUp)
         {
-
-            vector.x = vector.x + 0.7f; //makes the vector.x equal to itself plus 0.7f
-            equObject.transform.position = new Vector2(vector.x, vectorTwo.y); //Makes the position of the game object equal to the players position plus 0.7f on the x axis
-           // vectorTwo.y = vectorTwo.y + 1f; //makes the vector.x equal to itself plus 0.7f
+            vectorTwo.y = vectorTwo.y + 0.2f; //makes the vector.y equal to itself plus 0.2f
+            vector.x = vector.x + 0.7f;                                                 //makes the vector.x equal to itself plus 0.7f
+            equObject.transform.position = new Vector2(vector.x, vectorTwo.y);          //Makes the position of the game object equal to the players position plus 0.7f on the x axis and 0.2 on the y axis
         }
 
         if (Input.GetKey(KeyCode.Q) && pickedUp)
         {
-            equObject.transform.parent = null; //this removes the object from being a child to the player 
+            equObject.transform.parent = null;                                          //this removes the object from being a child to the player 
             pickedUp = false;
-            equObject.constraints = RigidbodyConstraints2D.None; // Un freezes the object so it can tilt over again.
-
+            equObject.constraints = RigidbodyConstraints2D.None;                        // Un freezes the object so it can tilt over again.
+            //equObject.GetComponent<Rigidbody2D>().gravityScale = 1; //Sets the gravity for that object to 0
         }
     }
 }
