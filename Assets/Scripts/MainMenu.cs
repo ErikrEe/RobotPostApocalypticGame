45bc91987ebfeac10ca6b4800632d3fabac0ec5d
ErikrEe <56590091+ErikrEe@users.noreply.github.com>
2020-01-13 10:57:30 +0100

    Made Transitions for menu --> Game
    Not done

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,14 +7,40 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
 
+    public Animator transition;
+
+    public float transitionTime = 1f;
+
+
     public void QuitGame() //This function closes the application when triggered
     {
         Application.Quit();
     }
+
+
+
+
+
     public void PlayGame() //This function starts the game by loading the first "Level" scene
     {
-        SceneManager.LoadScene("NewCodeScene");
+        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
+
+    IEnumerator LoadLevel(int levelIndex)
+    {
+        transition.SetTrigger("Start"); //Triggers the animation
+
+        yield return new WaitForSeconds(transitionTime); //Pauses Coroutine for set amount of seconds
+
+        SceneManager.LoadScene(levelIndex);
+    }
+
+
+
+
+    
+
+
     public void GoToOptions() //this function loads the "Options" scene
     {
         SceneManager.LoadScene("Options");
@@ -24,6 +50,8 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("MainMenu");
     }
+
+    
 
     //fixa en funktion som unloadar ESC MENU
     //fixa så att karaktären och allt annat är pausat under esc menu
