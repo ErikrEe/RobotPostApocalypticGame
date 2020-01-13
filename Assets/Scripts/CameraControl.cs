b3d2c81201dea24d5c2b3d62b631487fbd4584c2
GalnaGasen <jacob.kallberg@elev.ga.lbs.se>
2020-01-13 10:42:12 +0100

    Camera follow on trigger stay

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     public Camera Camera1;
-   // public GameObject Camera2;
+   public GameObject Camera2;
     public GameObject player;
     private Vector3 playerP;
     // Start is called before the first frame update
@@ -17,15 +17,38 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-           playerP = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 4);
+       
+       
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag("CameraSwitch"))
+        if (collision.CompareTag("CameraFollow"))
         {
+            cameraFollow();
             Camera1.transform.position = playerP;
-            Camera1.orthographicSize = 2;
+            Camera1.orthographicSize = 4;
+            Camera1.enabled = true;
+            Camera2.SetActive(false);
         }
+      if (collision.CompareTag("CameraSwitch"))
+        {
+
+          Camera1.enabled = false;
+          Camera2.SetActive(true);
+         
+
+        }
+       
+    }
+
+ 
+
+
+
+
+    void cameraFollow()
+    {
+        playerP = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z - 4);
     }
 }
