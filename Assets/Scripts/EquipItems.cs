0ca2ebfa69345c0486d2aa769beed3850e2c893b
Arex2 <56590254+Arex2@users.noreply.github.com>
2020-01-13 09:48:53 +0100

    Deletade flower scrip, kommenterade equitems

--- Assets/Scripts/EquipItems.cs
+++ Assets/Scripts/EquipItems.cs
@@ -12,98 +12,95 @@ public class EquipItems : MonoBehaviour
     Vector2 vector, vectorTwo;
     Rigidbody2D playerVelocity;
 
-    void Start()
+    void Start()                                                                              //Start is called before the first frame update
     {
         playerVelocity = GetComponent<Rigidbody2D>();
     }
-    // Update is called once per frame
-    void Update()
+
+    void Update()                                                                            //Update is called once per frame
     {
         float verticalVelocity = playerVelocity.velocity.y;
-        //vectorThree.y = Input.GetAxisRaw("Vertical");
 
 
         vector.x = gameObject.transform.position.x;
         vectorTwo.y = gameObject.transform.position.y;
+
         if (Input.GetKeyDown(KeyCode.E) && (gameObject.transform.position - this.transform.position).sqrMagnitude < 1.5f * 1.5f)
         {
             pickedUp = true;
-            vector.x = vector.x + 0.7f;                                                 //makes the vector.x equal to itself plus 0.7f
-            //vectorTwo.y = vectorTwo.y + 1f;                                             //makes the vector.x equal to itself plus 0.7f
-           // equObject.transform.position = new Vector2(vector.x, vectorTwo.y);          //Makes the position of the game object equal to the players position plus 0.7f on the x axis
+            vector.x = vector.x + 0.7f;                                                      //makes the vector.x equal to itself plus 0.7f
             equObject.transform.parent = gameObject.transform; 
             equObject.transform.rotation = Quaternion.Euler(0, 0, -90);
             equObject.constraints = RigidbodyConstraints2D.FreezeRotation;
-            //equObject.GetComponent<Rigidbody2D>().gravityScale = 0;
 
-            //GameObject.FindGameObjectsWithTag("equObject")
         }
+
         if(pickedUp)
         {
-            vectorTwo.y = vectorTwo.y + 0.2f; //makes the vector.y equal to itself plus 0.2f
-            vector.x = vector.x + 0.7f;                                                 //makes the vector.x equal to itself plus 0.7f
-                                                                                        //equObject.transform.position = new Vector2(vector.x, vectorTwo.y);          //Makes the position of the game object equal to the players position plus 0.7f on the x axis and 0.2 on the y axis
+            vectorTwo.y = vectorTwo.y + 0.2f;                                               //makes the vector.y equal to itself plus 0.2f
+            vector.x = vector.x + 0.7f;                                                     //makes the vector.x equal to itself plus 0.7f
+                                                                                            //equObject.transform.position = new Vector2(vector.x, vectorTwo.y);          //Makes the position of the game object equal to the players position plus 0.7f on the x axis and 0.2 on the y axis
 
-            // If the input is moving the player right and the player is facing left...
-            if (CharacterController.move < 0 && !CharacterController.facingRight)
+
+            if (CharacterController.move < 0 && !CharacterController.facingRight)           // If the input is moving the player right and the player is facing left...
             {
                 
-                // ... flip the player.
-                Flip();
-                //equObject.transform.position = new Vector2(vector.x, vectorTwo.y);   
+
+                Flip();                                                                     // ... flip the flower.
                 left = true;
                 right = false;
 
             }
-            // Otherwise if the input is moving the player left and the player is facing right...
-            else if (CharacterController.move > 0 && CharacterController.facingRight)
+
+            else if (CharacterController.move > 0 && CharacterController.facingRight)       // Otherwise if the input is moving the player left and the player is facing right...
             {
-                // ... flip the player.
-                Flip();
+                Flip();                                                                     // ... flip the flower.
                 left = false;
                 right = true;
-                //equObject.transform.position = new Vector2(-vector.x, vectorTwo.y);
-            }
-            if(pickedUp && PlayerMovement.jump)
-            {
-                if(playerVelocity.position.y < 0)
-                {
-                    gameObject.GetComponent<Rigidbody2D>().gravityScale = 0.2f; //Sets the gravity for that object to 0
-                }
-                gameObject.GetComponent<Rigidbody2D>().gravityScale = 1f; //Sets the gravity for that object to 1
-            
-            }
-        }
-        if(pickedUp && left)
-        {
-            vectorTwo.y = vectorTwo.y + 0.2f; //makes the vector.y equal to itself plus 0.2f
-            vector.x = vector.x - 1.5f;                                                 //makes the vector.x equal to itself plus 0.7f
-            equObject.transform.position = new Vector2(vector.x, vectorTwo.y);
 
+            }
+            if (left)
+            {
+                vectorTwo.y = vectorTwo.y + 0.2f;                                           //makes the vector.y equal to itself plus 0.2f
+                vector.x = vector.x - 1.5f;                                                 //makes the vector.x equal to itself plus 0.7f
+                equObject.transform.position = new Vector2(vector.x, vectorTwo.y);          //Makes the position of the object equal to the players position plus some modifications so that the object doesnt teleport into the player
+            }
+            else if (right)
+            {
+                vectorTwo.y = vectorTwo.y + 0.2f;                                           //makes the vector.y equal to itself plus 0.2f
+                vector.x = vector.x + 0.1f;                                                 //makes the vector.x equal to itself plus 0.7f
+                equObject.transform.position = new Vector2(vector.x, vectorTwo.y);          //Makes the position of the object equal to the players position plus some modifications so that the object doesnt teleport into the player
+            }
+
+
+             if(pickedUp && PlayerMovement.jump)
+             {
+                 if(playerVelocity.position.y < 0)
+                 {
+                     gameObject.GetComponent<Rigidbody2D>().gravityScale = 0.2f; //Sets the gravity for that object to 0
+                 }
+                 gameObject.GetComponent<Rigidbody2D>().gravityScale = 1f; //Sets the gravity for that object to 1
+
+             } 
         }
-        if(pickedUp && right)
-        {
-            vectorTwo.y = vectorTwo.y + 0.2f; //makes the vector.y equal to itself plus 0.2f
-            vector.x = vector.x + 0.1f;                                                 //makes the vector.x equal to itself plus 0.7f
-            equObject.transform.position = new Vector2(vector.x, vectorTwo.y);
-        }
+
+
+
 
         if (Input.GetKey(KeyCode.Q) && pickedUp)
         {
-            equObject.transform.parent = null;                                          //this removes the object from being a child to the player 
+            equObject.transform.parent = null;                                              //this removes the object from being a child to the player 
             pickedUp = false;
-            equObject.constraints = RigidbodyConstraints2D.None;                        // Un freezes the object so it can tilt over again.
-            gameObject.GetComponent<Rigidbody2D>().gravityScale = 1; //Sets the gravity for that object to 0
+            equObject.constraints = RigidbodyConstraints2D.None;                            // Un freezes the object so it can tilt over again
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 1;                        //Sets the gravity for that object to 1
         }
 
 
         void Flip()
         {
-            // Switch the way the player is labelled as facing.
-            CharacterController.facingRight = !CharacterController.facingRight;
-
-            // Multiply the player's x local scale by -1.
-            Vector3 theScale = transform.localScale;
+            
+            CharacterController.facingRight = !CharacterController.facingRight;             //Switch the way the player is labelled as facing.
+            Vector3 theScale = transform.localScale;                                        //Multiply the player's x local scale by -1.
             theScale.x *= -1;
             transform.localScale = theScale;
         }
