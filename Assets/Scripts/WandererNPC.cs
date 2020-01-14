e318a9de09c22817f9c213fdde0f59e3d435f7c5
ErikrEe <56590091+ErikrEe@users.noreply.github.com>
2020-01-14 12:24:28 +0100

    Changed controls to only WASD + other stuff
    - Changed the npc script so that now, moving right or left has a higher chance than standing still
    
    - Changed the controls so that now you can only use W A S D , since there was a bug with jumping 2x or even 3x higher when "space" + "up" was available to use as jump buttons
    
    -General fixes

--- Assets/Scripts/WandererNPC.cs
+++ Assets/Scripts/WandererNPC.cs
@@ -14,8 +14,8 @@ public class WandererNPC : MonoBehaviour
     public Vector2 decisionTime = new Vector2(1, 4);
     internal float decisionTimeCount = 0;
 
-    // The possible directions that the object can move int, right, left, up, down, and zero for staying in place. I added zero twice to give a bigger chance if it happening than other directions
-    internal Vector3[] moveDirections = new Vector3[] { Vector3.right, Vector3.left, Vector3.zero, Vector3.zero };
+    // The possible directions that the object can move int, right, left, up, down, and zero for staying in place. I added left & right twice to give a bigger chance if it happening
+    internal Vector3[] moveDirections = new Vector3[] { Vector3.right, Vector3.left, Vector3.right, Vector3.left, Vector3.zero };
     internal int currentMoveDirection;
 
     // Use this for initialization
