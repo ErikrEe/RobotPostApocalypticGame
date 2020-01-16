7066085cbb1e2f3ae948a58dc973c67c2ce9317a
ErikrEe <56590091+ErikrEe@users.noreply.github.com>
2020-01-16 17:28:15 +0100

    camera almost workin

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -21,14 +21,14 @@ public class CameraControl : MonoBehaviour
     }
     private void OnTriggerStay2D(Collider2D collision)
     {
-       /* if (collision.CompareTag("CameraFollow"))   // Checks if the player is colliding with the trigger
+        if (collision.CompareTag("CameraFollow"))   // Checks if the player is colliding with the trigger
         {
             cameraFollow();       //if player is colliding, start the camerafollow method
             Camera1.transform.position = playerP;   // Cameras position switches to players position
             Camera1.orthographicSize = Mathf.Lerp(Camera1.orthographicSize, CameraZoomIn, Time.deltaTime);    //zoom in the camera smoothly
     
         }
-        */
+        
       if (collision.CompareTag("CameraSwitch1"))   //Checks if the player is colliding with the trigger
         {
 
