623a2116d7a98f9c59f93017296665319a7a388e
Arex2 <56590254+Arex2@users.noreply.github.com>
2020-03-20 19:14:22 +0100

    commenting FlowerCollider script

--- Assets/Scripts/FlowerCollider.cs
+++ Assets/Scripts/FlowerCollider.cs
@@ -4,49 +4,32 @@ using UnityEngine;
 
 public class FlowerCollider : MonoBehaviour
 {
-
+    //Harriet's script, used to turn a collider on and off, when picking up and dropping the game object called Flower
+    //Finding the right collider to use in the script
     public Collider2D collider;
 
     void Start()
     {
-        //collider.enabled = false;
+        //The collider is active as a trigger preventing it from looking as if it's collliding with objects
         collider.isTrigger = true;
     }
 
 
     void FixedUpdate()
     {
-
-        // Cast a ray to the right.
-        // RaycastHit2D hit = Physics2D.Raycast((transform.position), Vector2.right, 2f); //,11
-
-        // If it hits something...
-        if (EquipItems.pickedUp) // && hit.collider != null
+        // If the flower is picked up...
+        if (EquipItems.pickedUp) 
         {
-            //movement speed = 0
-            //PlayerMovement.moveSpeed = 0;
-            //Toggle the Collider on and off when pressing the space bar
-            //collider.enabled = true;
-            //collider = collider.enabled;
-            collider.isTrigger = false;
-
+            collider.isTrigger = false; //then the trigger is not enabled -> making the collider actually collide
 
         }
-        else if (!EquipItems.pickedUp) //DEN FLIPPAR INTE!!!!! //hit.collider == null || 
+        //If the flower is not picked up...
+        else if (!EquipItems.pickedUp) 
         {
-            //movement speed = movement speed
-            //PlayerMovement.moveSpeed = 40f;
-            //collider.enabled = false;
-            //collider = !collider.enabled;
+            //then the collider is used as a trigger -> making the collider NOT collide with objects
             collider.isTrigger = true;
-
-
         }
-        /* if (Input.GetKeyDown(KeyCode.E))
-         {
-             //Toggle the Collider on and off when pressing e
-             collider.enabled = !collider.enabled;
-         }*/
+
 
     }
 }
