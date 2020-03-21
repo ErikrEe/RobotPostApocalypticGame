2366debd8813d21cf03a0e37a65b647008cd6a82
Arex2 <56590254+Arex2@users.noreply.github.com>
2020-03-21 13:43:12 +0100

    EquipItems Script fully cleaned!

--- Assets/Scripts/EquipItems.cs
+++ Assets/Scripts/EquipItems.cs
@@ -2,20 +2,22 @@
 
 public class EquipItems : MonoBehaviour
 {
-    //Harriet's script, used to pick up and drop a flower object with "e", as well as drag and drop boxes with "e"
+    //Harriet's script, used to pick up and drop a flower object with "e", as well as drag/push and drop boxes with "e"
     [SerializeField]
     public Rigidbody2D equObject;
     public static Rigidbody2D playerVelocity, objectRigidbody;
     GameObject dragObject;
-    //dessa avänds till att kalkylera spelarens storlek på X axeln
+
+    //these are used to calculate the players size on the x axis
     Collider2D playerCollider, objectCollider;
+    //these are used to determine where the flower and drag objects should be when the player is interacting with them
     Vector2 vector, vectorTwo;
 
     public static bool pickedUp, objectDraged, objectLeft, objectRight, open, open2 = false;
     private float currentheight, previousheight, travel, offset;
     private bool left, right = false;
 
-    public Animator animator;
+    public Animator animator; //Erik,
 
     //Start is called before the first frame update
     void Start()
@@ -100,7 +102,7 @@ public class EquipItems : MonoBehaviour
             if (objectLeft)
             {
                 //makes the vector.x equal to itself minus the choosen offset
-                vector.x = vector.x - offset;;
+                vector.x = vector.x - offset; ;
             }
 
             //sets the dragObjects position equal to the variable vector.x and the dragObjects y axis position
@@ -112,107 +114,116 @@ public class EquipItems : MonoBehaviour
         #region Picking up the flower
         if (Input.GetKeyDown(KeyCode.E))
         {
+            //open2 becomes !open2, allowing the use of e, for both picking up and dropping the flower object
             open2 = !open2;
+
+            //if open2 is true and the equObject is within a 2f * 2f radius and objectDraged is false...
             if (open2 && (equObject.transform.position - this.transform.position).sqrMagnitude < 2f * 2f && objectDraged == false)
             {
                 pickedUp = true;
-                animator.SetBool("HoldingFlower", true);  //Sets the "HoldingFlower" bool to true
-                vector.x = vector.x + 0.7f;                                                      //makes the vector.x equal to itself plus 0.7f
+                animator.SetBool("HoldingFlower", true); //Erik, Sets the "HoldingFlower" bool to true
 
-
-
+                //gives the equObject all possible constraints (preventing it from sliding, falling, tilting etc)
                 equObject.constraints = RigidbodyConstraints2D.FreezeAll;
+                //makes the equObject a child of the gameObject
                 equObject.transform.parent = gameObject.transform;
-                equObject.transform.position = new Vector2(vector.x, vectorTwo.y);              //Makes the position of the object equal to the players position plus some modifications so that the object doesnt teleport into the player
+                //rotates the equObject to a rotation of 0,0,0
                 equObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
 
-
-                if (equObject.transform.localScale.x < 0) //Tobbes kod
+                //if the equObject moves negatively on the x axis..
+                if (equObject.transform.localScale.x < 0)
                 {
-                    Vector3 theScale = equObject.transform.localScale;                          //Multiply the player's x local scale by -1.
+                    //multiply the player's x local scale by -1
+                    Vector3 theScale = equObject.transform.localScale;
                     theScale.x *= -1;
                     equObject.transform.localScale = theScale;
                 }
             }
-            if (!open2)
-            {
-                if (pickedUp)
-                {
 
-                    equObject.transform.parent = null;                                              //this removes the object from being a child to the player 
-                    pickedUp = false;
-
-
-                    animator.SetBool("HoldingFlower", false);
-
-                    equObject.constraints = RigidbodyConstraints2D.None;                            // Un freezes the object so it can tilt over again
-                    gameObject.GetComponent<Rigidbody2D>().gravityScale = 3;                        //Sets the gravity for the player to 3 (3 is the default gravity scale for the player)
-
-
-                }
-            }
         }
-
+        //if pickedUp is true...
         if (pickedUp)
         {
-            vectorTwo.y = vectorTwo.y + 1.55f;                                               //makes the vector.y equal to itself plus 1.55f
-            vector.x = vector.x + 0.65f;                                                     //makes the vector.x equal to itself plus 0.65f                                                                                           
+            //makes the vector.y equal to itself plus 0.65f
+            vectorTwo.y = vectorTwo.y + 0.65f;
+            //makes the vector.x equal to itself plus 0.60f  
+            vector.x = vector.x + 0.6f;
 
-            if (CharacterController.move < 0 && !CharacterController.facingRight)           // If the input is moving the player right and the player is facing left...
+            //If the input is moving the player right and the player is facing left...
+            if (CharacterController.move < 0 && !CharacterController.facingRight)
             {
                 left = true;
                 right = false;
-
             }
 
-            else if (CharacterController.move > 0 && CharacterController.facingRight)       // Otherwise if the input is moving the player left and the player is facing right...
+            // Otherwise if the input is moving the player left and the player is facing right...
+            else if (CharacterController.move > 0 && CharacterController.facingRight)
             {
-
                 left = false;
                 right = true;
-
             }
-            equObject.transform.position = new Vector2(vector.x, vectorTwo.y);          //Makes the position of the object equal to the players position plus some modifications so that the object doesnt teleport into the player
+
+            //if the flower should be on the left side...
             if (left)
             {
-                vectorTwo.y = vectorTwo.y - 0.8f; //0.2f;                                           //makes the vector.y equal to itself plus 0.2f
-                vector.x = vector.x - 1.2f;                                                 //makes the vector.x equal to itself plus 0.7f
+                //makes the vector.x equal to itself minus 1.2f
+                vector.x = vector.x - 1.2f;
             }
+            //if the flower should be on the right side...
             if (right)
             {
-                vectorTwo.y = vectorTwo.y - 0.8f;                                           //makes the vector.y equal to itself plus 0.2f
-                vector.x = vector.x + 0.1f;                                                 //makes the vector.x equal to itself plus 0.7f
+                //makes the vector.x equal to itself plus 0.0f
+                vector.x = vector.x + 0.0f;
             }
-            equObject.transform.position = new Vector2(vector.x, vectorTwo.y);              //Makes the position of the object equal to the players position plus some modifications so that the object doesnt teleport into the player
+
+            //Makes the position of the object equal to the players position plus some modifications so that the object doesnt teleport into the player
+            equObject.transform.position = new Vector2(vector.x, vectorTwo.y);
+
+            //if open2 is false..
+            if (!open2)
+            {
+
+                //the equObject becomes a child of nothing 
+                equObject.transform.parent = null;                                              
+                pickedUp = false;
+
+                animator.SetBool("HoldingFlower", false); //Erik, Sets the "HoldingFlower" bool to false
+
+                //Removes the equObjects constraints so it can tilt over again and be affected by other objects in the scene
+                equObject.constraints = RigidbodyConstraints2D.None;
+                //Sets the gravity for the player to 3 (3 is the default gravity scale for the player)
+                gameObject.GetComponent<Rigidbody2D>().gravityScale = 3;                       
+
+            }
         }
         #endregion
 
     }
 
     //This part of the code determines if the player is moving upwards or downwards on the y axis by comparing past and current position.
-    void LateUpdate() 
+    void LateUpdate()
     {
 
         currentheight = transform.position.y;
         travel = currentheight - previousheight;
         //if the player is moving negatively on the y axis...
-        if (pickedUp && travel < 0f)                                                        
+        if (pickedUp && travel < 0f)
         {
             //Then the gravity for that object is set to 0.2f
-            gameObject.GetComponent<Rigidbody2D>().gravityScale = 0.2f;                     
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 0.2f;
         }
         //if the player is moving positively on the y axis...
-        else if (pickedUp && travel > 0)                                                    
+        else if (pickedUp && travel > 0)
         {
             //Then the gravity for that object is set to 3 (which is the default value we are using)
-            gameObject.GetComponent<Rigidbody2D>().gravityScale = 3f;                       
+            gameObject.GetComponent<Rigidbody2D>().gravityScale = 3f;
         }
         previousheight = currentheight;
     }
 
     #region finds and assigns the dragObject variable, objectCollider and objectRigidbody
     //finds which object with the "enemy" script equipped is closest to the player
-    void FindClosestEnemy() 
+    void FindClosestEnemy()
     {
         float distanceToClosestEnemy = Mathf.Infinity;
         Enemy closestEnemy = null;
