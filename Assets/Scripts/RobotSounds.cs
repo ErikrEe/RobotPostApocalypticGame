5ebe2d9dcbe2c71c5faae7d63528c03d5f603c04
Arex2 <56590254+Arex2@users.noreply.github.com>
2020-03-24 08:31:32 +0100

    cleaning scripts

---
+++ Assets/Scripts/RobotSounds.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotSounds : MonoBehaviour
+{
+
+    //lägger in componenten själv
+    [SerializeField]
+    AudioSource robotMovement, ambient1, ambient2, ambient3;
+
+    int intNumber;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //if the player/robot is on the ground and moves...
+        if (CharacterController.grounded && (CharacterController.move < 0 || CharacterController.move > 0))
+        {
+            //if the robotMovement is not playing...
+            if (!robotMovement.isPlaying)
+            {
+                //play the robot sound
+                robotMovement.Play(0);
+
+            }
+        }
+        else
+        {
+            //stop playing the robotMovement sound
+            robotMovement.Stop();
+        }
+
+        //## LÄGGA TILL RANDOM WAIT TIME MELLAN ALLT SÅ ATT LJUDEN SPELAS AT RANDOM POINTS!
+        //if the intNumber is equal to 1, if the intNumber is equal to 2, if the intNumber is equal to 3...
+        if (intNumber == 1)
+        {
+            ambient1.Play(0);
+        }
+        else if (intNumber == 2)
+        {
+            ambient2.Play(0);
+        }
+        else if (intNumber == 3)
+        {
+            ambient3.Play(0);
+        }
+
+    }
+
+    //This function chooses a random number between 1-3 whenever it is called, and sets the intNumber equal to the randomly picked number
+    int GetRandom(int min, int max)
+    {
+        min = 1;
+        max = 3;
+        int rand = Random.Range(min, max);
+
+        rand = intNumber;
+        return rand;
+    }
+
+}
---
+++ Assets/Scripts/VolOptions.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+public class VolOptions : MonoBehaviour
+{
+
+    //to find the audio mixers and sliders
+    public AudioMixer mixer;
+    public Slider slider;
+    public AudioMixer mixerTwo;
+    public Slider sliderTwo;
+
+
+    // Start is called before the first frame update
+    void Start() //here the audio and music variables are loaded with a deafult float of 0.75f 
+    {
+        slider.value = PlayerPrefs.GetFloat("AudioVolume", 0.75f);
+        sliderTwo.value = PlayerPrefs.GetFloat("MusicVolume", 0.75f);
+        SetMusic(PlayerPrefs.GetFloat("MusicVolume", 0.75f));
+        SetVolume(PlayerPrefs.GetFloat("AudioVolume", 0.75f));
+    }
+
+    public void SetVolume(float sliderValue) //this function sets and saves the audio volume
+    {
+        mixer.SetFloat("AudioVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("AudioVolume", sliderValue);
+    }
+    public void SetMusic(float sliderValue)//this function sets and saves the music volume
+    {
+        mixerTwo.SetFloat("MusicVol", Mathf.Log10(sliderValue) * 20);
+        PlayerPrefs.SetFloat("MusicVolume", sliderValue);
+    }
+
+}
