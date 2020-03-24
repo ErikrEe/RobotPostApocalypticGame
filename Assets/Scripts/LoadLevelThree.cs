f12a9cbd5f468cc978db486f0742e48b8b73d001
ErikrEe <56590091+ErikrEe@users.noreply.github.com>
2020-03-24 14:26:29 +0100

    Small adjustments

--- Assets/Scripts/LoadLevelThree.cs
+++ Assets/Scripts/LoadLevelThree.cs
@@ -9,6 +9,12 @@ public class LoadLevelThree : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collision)
     {
         SceneManager.LoadScene(4);
+
+        EquipItems.pickedUp = false;
+        EquipItems.objectDraged = false;            //When Loading the level, the variables that check certain conditions...
+        EquipItems.objectLeft = false;              //...(flower being picked up, dragging / pushing objects... 
+        EquipItems.objectRight = false;             //...and facing directions) will be reset - Erik
+        CharacterController.facingRight = true;
     }
 
 
